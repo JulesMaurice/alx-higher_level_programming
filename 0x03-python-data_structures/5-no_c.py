3881fbff92becdb3637ae23f0d7c720e88172836
JulesMaurice <julesmauricemuhirecyiza.com>
2023-05-15 12:16:25 -0700

    remove all characters in python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
-#def no_c(my_string):
-#    newStr = {99:None}
-#    nz = {67:None}
-#    newest =  my_string.translate(newStr)
-#    rsStr = newest.translate(nz)
-#    return rsStr
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
+
